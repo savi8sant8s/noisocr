227eac8759ef884d1452db03b91ad753002ef358
Sávio Santos <savi8sant8s@gmail.com>
2024-09-08 11:50:47 -0300

    Release 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='noisocr',
-    version='0.1',
+    version='0.2',
     description='Tools to simulate post-OCR noisy texts.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
